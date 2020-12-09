067445cb5d04cbaa307c84840a2117a044f849ed
Angel Mendez <angelmendezz751@gmail.com>
2020-12-08 19:53:26 -0600

    Add label

--- src/components/Buttons/CounterSelector/index.jsx
+++ src/components/Buttons/CounterSelector/index.jsx
@@ -1,8 +1,11 @@
 export const CounterSelector = ({handleChange, defaultValue}) => {
     return (
         <div className="w-full flex items-center">
-            <div className="w-4/12">Cantidad</div>
+            <div className="w-4/12">
+                <label htmlFor="add_to_cart[select_quantity]">Cantidad</label>
+            </div>
             <select
+                id="add_to_cart[select_quantity]"
                 onChange={handleChange}
                 className="w-8/12 focus:ring-indigo-500 focus:border-indigo-500 h-full py-3 pl-4 font-medium text-black pr-6 border-transparent bg-gray-100 pr-6 select appearance-none rounded-md">
                 <option value="1">1</option>
