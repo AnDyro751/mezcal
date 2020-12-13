4d4a5164ebffc7962b97852703da90a2b6b74dc8
Angel Mendez <angelmendezz751@gmail.com>
2020-12-12 18:29:20 -0600

    Init new header

--- src/components/Headers/Public/index.jsx
+++ src/components/Headers/Public/index.jsx
@@ -6,33 +6,34 @@ import {SITE_TITLE} from "../../../site/info";
 export default function HeadersPublic() {
     const [state, dispatch] = useContext(OrderContext);
     const {order} = state;
-    // useEffect(() => {
-    //     console.log(order);
-    // }, [state.order]);
     return (
-        <header className="w-full">
-            <div className="grid grid-cols-3">
-                <div className="col-span-2 grid grid-cols-2">
-                    <div className="col-span-1">
-                        <Link href={"/"}>
-                            <a>{SITE_TITLE}</a>
-                        </Link>
+        <div className="w-full flex justify-center py-3 border-b">
+            <div className="w-10/12 flex items-center">
+                <header className="w-full">
+                    <div className="grid grid-cols-3 items-center">
+                        <div className="col-span-2 grid grid-cols-2">
+                            <div className="col-span-1">
+                                <Link href={"/"}>
+                                    <a className="text-4xl font-medium" >{SITE_TITLE}</a>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="col-span-1">
+                            <Link href={"/products"}>
+                                <a>Productos</a>
+                            </Link>
+                            |
+                            <Link href={"/products/demo-1"}>
+                                <a>Producto</a>
+                            </Link>
+                            |
+                            <Link href={"/cart"}>
+                                <a>Carrito({order.itemCount || 0})</a>
+                            </Link>
+                        </div>
                     </div>
-                </div>
-                <div className="col-span-1">
-                    <Link href={"/products"}>
-                        <a>Productos</a>
-                    </Link>
-                    |
-                    <Link href={"/products/demo-1"}>
-                        <a>Producto</a>
-                    </Link>
-                    |
-                    <Link href={"/cart"}>
-                        <a>Carrito({order.itemCount || 0})</a>
-                    </Link>
-                </div>
+                </header>
             </div>
-        </header>
+        </div>
     )
 }
