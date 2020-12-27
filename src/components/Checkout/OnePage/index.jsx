001b646b7b6888349230186029d5008761e07c74
Angel Mendez <angelmendezz751@gmail.com>
2020-12-27 13:36:21 -0600

    Init stepper icon

--- src/components/Checkout/OnePage/index.jsx
+++ src/components/Checkout/OnePage/index.jsx
@@ -6,7 +6,7 @@ import OnePageDataCheckout from "./Data";
 export default function ComponentsCheckoutOnePage({}) {
     return (
         <div className="w-10/12 justify-between mx-auto flex mt-10 space-x-5">
-            <div className="w-6/12">
+            <div className="w-7/12">
                 <OnePageAddressForm/>
             </div>
             <div className="w-4/12">
