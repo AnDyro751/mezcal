97b76e71c6c2b568d9ab0d54db1b0cc319eb9343
Angel Mendez <angelmendezz751@gmail.com>
2020-12-30 10:51:14 -0600

    Actualizamos el envío y mandamos al next state

--- src/components/Checkout/ShippingSelected/index.jsx
+++ src/components/Checkout/ShippingSelected/index.jsx
@@ -1,14 +1,7 @@
-import {useContext, useMemo, useState} from "react";
-import ButtonsPrimary from "../../Buttons/primary";
-import {OrderContext} from "../../../stores/userOrder";
-import {useMutation} from "@apollo/client";
-import NEXT_STATE_MUTATION from "../../../graphql/mutations/cart/nextState";
-import {useToasts} from "react-toast-notifications";
+import {useMemo, useState} from "react";
 
 export default function CheckoutShippingSelected({shipping}) {
     const [shippingRateSelected, setShippingRate] = useState(null);
-    const {state, dispatch} = useContext(OrderContext);
-    const {addToast} = useToasts();
 
     useMemo(() => {
         let currentSelected = shipping.shippingRates.nodes.find((el) => el.selected === true);
@@ -18,26 +11,7 @@ export default function CheckoutShippingSelected({shipping}) {
         // })
     }, [])
 
-    const [toNextState, {data, loading, error}] = useMutation(NEXT_STATE_MUTATION, {
-        onCompleted: (newData) => {
-            if (newData.nextCheckoutState.errors.length > 0) {
-                addToast(newData.nextCheckoutState.errors[0].message, {
-                    appearance: "error"
-                })
-            } else {
-                dispatch({type: "UPDATE_ORDER", payload: {...state.order, ...newData.nextCheckoutState.order}});
-            }
-        }
-    })
 
-    const handleClick = () => {
-        if (state.order.state === "delivery") {
-            toNextState();
-            console.log("Vamos a hacer next");
-        } else {
-            alert(state.order.state)
-        }
-    }
     return (
         <div className="w-full">
             Método de envío seleccionado
@@ -51,9 +25,7 @@ export default function CheckoutShippingSelected({shipping}) {
                     :
                     <p>Selecciona un método de envío</p>
             }
-            <ButtonsPrimary onClick={handleClick}
-                            text={"Siguiente"}
-            />
+
         </div>
     )
 }
