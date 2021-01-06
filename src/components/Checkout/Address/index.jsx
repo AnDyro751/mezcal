07ec8af040101bca59629807f45388d4b4efaf1e
Angel Mendez <angelmendezz751@gmail.com>
2021-01-05 23:41:59 -0600

    Actualizamos la orden

--- src/components/Checkout/Address/index.jsx
+++ src/components/Checkout/Address/index.jsx
@@ -4,10 +4,13 @@ import {useFormik} from "formik";
 import validate from "../OnePage/validateValues";
 import {useContext} from "react";
 import {OrderContext} from "../../../stores/userOrder";
-import {isEqual} from "lodash";
+import {useMutation} from "@apollo/client";
+import {useToasts} from "react-toast-notifications";
+import ADD_ADDRESS_TO_CHECKOUT_MUTATION from "../../../graphql/mutations/cart/addAddressToCheckout";
 
 export default function CheckoutAddress({data}) {
     const {state, dispatch} = useContext(OrderContext);
+    const {addToast} = useToasts();
 
     const formik = useFormik({
         initialValues: {
@@ -30,13 +33,66 @@ export default function CheckoutAddress({data}) {
         },
     });
 
+    const [addAddressToOrder, {data: dataAddressToOrder, loading: loadingToOrder, error: errorToOrder}] = useMutation(ADD_ADDRESS_TO_CHECKOUT_MUTATION, {
+        variables: {
+            input: {
+                billingAddress: {
+                    address1: formik.values.address1,
+                    address2: formik.values.address2,
+                    city: formik.values.city,
+                    countryId: data.countryByIso ? data.countryByIso.id : "",
+                    firstname: formik.values.firstname,
+                    lastname: formik.values.lastname,
+                    phone: formik.values.phone,
+                    stateId: formik.values.stateId,
+                    zipcode: formik.values.zipcode
+                },
+                shippingAddress: {
+                    address1: formik.values.address1,
+                    address2: formik.values.address2,
+                    city: formik.values.city,
+                    countryId: data.countryByIso ? data.countryByIso.id : "",
+                    firstname: formik.values.firstname,
+                    lastname: formik.values.lastname,
+                    phone: formik.values.phone,
+                    stateId: formik.values.stateId,
+                    zipcode: formik.values.zipcode
+                }
+            }
+        },
+        onCompleted: (newDataAddress) => {
+            console.log("NEW", newDataAddress.addAddressesToCheckout)
+            if (newDataAddress.addAddressesToCheckout.errors.length > 0) {
+                addToast(newDataAddress.addAddressesToCheckout.errors[0].message, {
+                    appearance: 'error'
+                });
+            } else {
+                dispatch({
+                    type: "UPDATE_ORDER",
+                    payload: {...state.order, ...newDataAddress.addAddressesToCheckout.order}
+                });
+                if (newDataAddress.addAddressesToCheckout.order.state === "address") {
+                    console.log("VAMOS A NEXT");
+                    // toNext();
+                }
+            }
+        },
+        onError: (e) => {
+            addToast(e.message ? e.message : e, {
+                appearance: 'error'
+            });
+        }
+    });
+
     const handleSendForm = () => {
         if (state.order.email === formik.values.email) {
-            console.log("UPDATE 1")
+            console.log("UPDATE 1");
+            addAddressToOrder();
         } else {
             console.log("UPDATE 2")
         }
     }
+
 
     const onHandleChange = (name, value) => {
     };
