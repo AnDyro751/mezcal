fdc59300d1da2bba4e47d3043f8bb014d0a8dc96
Angel Mendez <angelmendezz751@gmail.com>
2021-01-09 22:52:29 -0600

    Init taxonomy & taxon pages

---
+++ pages/t/[slug].jsx
@@ -0,0 +1,7 @@
+export default function PagesTaxonomy() {
+    return (
+        <div className="w-full">
+            Taxonomy
+        </div>
+    )
+}
---
+++ pages/t/[slug]/[taxon].jsx
@@ -0,0 +1,7 @@
+export default function PagesTaxon() {
+    return (
+        <div className="w-full">
+            Taxooon
+        </div>
+    )
+}
